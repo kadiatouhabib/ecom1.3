7d3049089de7ec3c73adf39ebab4419180b6c06b
Kadiatouhabib <khadeejah224@gmail.com>
2023-09-27 17:55:35 -0400

    add form and result

---
+++ index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+    <body>
+        <form method="post" action="result.php">
+        Name:
+        <input type = "text" name = "fname">
+        <input type = "submit">
+        </form>
+    </body>
+</html>
